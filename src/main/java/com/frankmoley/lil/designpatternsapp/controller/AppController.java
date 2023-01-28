5195ab0128f7221fe6f2ade1a98196670f623312
Bogdan Mierloiu <bogdy.musiclover@gmail.com>
2023-01-28 02:03:05 +0200

    Factory pattern - create Pet

--- src/main/java/com/frankmoley/lil/designpatternsapp/controller/AppController.java
+++ src/main/java/com/frankmoley/lil/designpatternsapp/controller/AppController.java
@@ -2,6 +2,8 @@ package com.frankmoley.lil.designpatternsapp.controller;
 
 
 import com.frankmoley.lil.designpatternsapp.factory.Pet;
+import com.frankmoley.lil.designpatternsapp.factory.PetFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+
 public class AppController {
+    @Autowired
+    private PetFactory petFactory;
 
     @GetMapping
-    public String getDefault(){
+    public String getDefault() {
         return "{\"message\": \"Hello World\"}";
+    }
+
+    @PostMapping("adoptPet/{type}/{name}")
+    public Pet adoptPet(@PathVariable String type, @PathVariable String name) {
+        Pet pet = this.petFactory.createPet(type);
+        pet.setName(name);
+        pet.feed();
+        return pet;
     }
 
 }
--- src/main/java/com/frankmoley/lil/designpatternsapp/factory/PetFactory.java
+++ src/main/java/com/frankmoley/lil/designpatternsapp/factory/PetFactory.java
@@ -4,4 +4,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PetFactory {
+
+    public Pet createPet(String animalType){
+        switch (animalType.toLowerCase()){
+            case "dog" :
+                return new Dog();
+            case "cat" :
+                return new Cat();
+            default:
+                throw new UnsupportedOperationException("unknown animal type");
+        }
+    }
 }
